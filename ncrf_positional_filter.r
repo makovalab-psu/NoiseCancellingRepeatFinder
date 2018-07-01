b27a57ed368db01bba8f6af20b2570952a7a6371
Bob Harris <rsharris@bx.psu.edu>
2018-07-01 15:32:51 -0400

    added verbose option to do_mx_significance_tests; added discrtiption of some of the options

--- ncrf_positional_filter.r
+++ ncrf_positional_filter.r
@@ -4,8 +4,19 @@
 
 require(pwr)
 
-do_mx_significance_tests <- function(n,mxFlat,testErrorCounts,effectSize=0.3,power=0.8)
-	{ 
+do_mx_significance_tests <- function(n,mxFlat,testErrorCounts,effectSize=0.3,power=0.8,verbose=F)
+	{
+	# n
+	#	is the number of flattened rows (the number of tests to perform)
+	# mxFlat
+	#	is a flattened matrix of the test values; each flattened row has 2*k
+	#	columns, where k is the vector size of a test; thus each flattened row
+	#   has two test vectors, which for our original purposes were "match
+	#	counts" and "error counts"
+	# testErrorCounts
+	#	is True  if we're to test the second test vector in each flattened row
+	#	is False if we're to test the first  test vector in each flattened row
+
 	rowLen = length(mxFlat)/n
 	for (row in 1:n)
 		{
@@ -21,7 +32,9 @@ do_mx_significance_tests <- function(n,mxFlat,testErrorCounts,effectSize=0.3,pow
 			s = (row-1)*rowLen + 1
 			e = s + (rowLen/2) - 1
 			}
-		cat(assess_significance(mxFlat[s:e],effectSize=effectSize,power=power))
+		if (verbose)
+			write(paste("row:",row,"events:",paste(mxFlat[s:e],collapse=" ")), stderr())
+		cat(assess_significance(mxFlat[s:e],effectSize=effectSize,power=power,verbose=verbose))
 		cat('\n')
 		}
 	}
@@ -45,7 +58,7 @@ assess_significance <- function(case,effectSize=0.3,power=0.8,verbose=F)
 		# Calculating alpha value for fixed power
 		alpha <- pwr.chisq.test(w=effectSize,df=df,N=sampleSize,power=power,sig.level=NULL)$sig.level
 		,
-		# But if an error occurs, we will stick to the default alpha value. 
+		# But if an error occurs, we will stick to the default alpha value.
 		# This happens when the alpha value becomes extremely small.
 		error=function(error_message)
 			{
