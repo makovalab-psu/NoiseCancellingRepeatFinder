330cec9394f2088f3d395b7fb99464cca3999067
Bob Harris <rsharris@bx.psu.edu>
2018-07-15 14:22:55 -0400

    added --method=min-max, not well-tested yet

--- error_uniformity_filter.py
+++ error_uniformity_filter.py
@@ -10,7 +10,11 @@ don't (seem to) have uniformly distributed matches across the motif unit.
 import subprocess
 from sys        import argv,stdin,stdout,stderr,exit
 from os         import path as os_path
+from random     import seed as random_seed,sample as random_sample,randint
 from ncrf_parse import alignments,parse_probability,int_with_unit
+
+defaultPrngSeed = "NCRF.error_uniformity_filter"
+
 
 # Implementation Notes:
 #
@@ -38,11 +42,16 @@ from ncrf_parse import alignments,parse_probability,int_with_unit
 def usage(s=None):
 	message = """
 usage: cat <output_from_NCRF> | error_uniformity_filter [options]
+  --method=chi-squared  judge alignments by a chi-squared test
+                        (this is the default)
+  --method=min-max      judge alignments by a "min-max" test  
   --effectsize=<value>  effect size for chi-squared test
                         (default is 0.3)
   --power=<probability> "power of test" for chi-squared test, 1 minus Type II
                         error probability
                         (default is 0.8)
+  --trials=<number>     number of trials for min-max-test
+                        (default is 100)
   --discard:good        discard the "good" alignments instead of the "bad" ones
                         (by default we discard the "bad" alignments) 
   --discard:none        don't discard any alignments, just report the test
@@ -54,6 +63,10 @@ usage: cat <output_from_NCRF> | error_uniformity_filter [options]
                         some alignments
   --test:matches-insertions perform the test using match counts less insertions
   --warn:untested       report each untestable alignment as a warning
+  --seed=<string>       set random seed; this is only applicable to the min-max
+                        test; two special cases are "default" (use the built-in
+                        default seed) and "none" (don't seed the random number
+                        generator)
   --head=<number>       limit the number of input alignments
   --batch=<number>      number of input alignments processed by each call to R;
                         our ability to call R fails if the command line we pass
@@ -90,8 +103,10 @@ def main():
 
 	# parse the command line
 
+	testMethod     = "chi-squared"
 	effectSize     = 0.3
 	power          = 0.8
+	numTrials      = 100
 	discardWhich   = "bad"
 	testWhich      = "matches"
 	warnOnUntested = False
@@ -99,16 +114,23 @@ def main():
 	batchSize      = 30
 	reportAs       = "ncrf"
 	requireEof     = True
+	prngSeed       = defaultPrngSeed
 	debug          = []
 
 	for arg in argv[1:]:
 		if ("=" in arg):
 			argVal = arg.split("=",1)[1]
 
-		if (arg.startswith("--effectsize=")):
+		if (arg in ["--method=chi-squared","--method=chi-square"]):
+			testMethod = "chi-squared"
+		elif (arg == "--method=min-max"):
+			testMethod = "min-max"
+		elif (arg.startswith("--effectsize=")):
 			effectSize = parse_probability(argVal)
 		elif (arg.startswith("--power=")):
 			power = parse_probability(argVal)
+		elif (arg.startswith("--trials=")):
+			numTrials = int(argVal)
 		elif (arg in ["--discard:bad","--discard=bad"]):
 			discardWhich = "bad"
 		elif (arg in ["--discard:good","--discard=good"]):
@@ -132,6 +154,22 @@ def main():
 			reportAs = "matrix"
 		elif (arg in ["--noendmark]","--noeof","--nomark"]):   # (unadvertised)
 			requireEof = False
+		elif (arg.startswith("--seed=")):
+			seed = argVal
+			if (seed in ["none","None","NONE"]):
+				prngSeed = None
+			elif (seed in ["default","Default","DEFAULT"]):
+				prngSeed = defaultPrngSeed
+			else:
+				# nota bene: if the seed is a number, use it as a number, since
+				#            string seeds can produce different sequences on
+				#            different versions/builds of python
+				try:
+					seed = int(seed)
+				except ValueError:
+					try:               seed = float(seed)
+					except ValueError: pass
+				prngSeed = seed
 		elif (arg == "--debug"):
 			debug += ["debug"]
 		elif (arg.startswith("--debug=")):
@@ -144,48 +182,32 @@ def main():
 	if (reportAs == "matrix"):
 		discardWhich = "none"
 
+	# initialize the PRNG, if needed
+
+	if (testMethod == "min-max"):
+		if (prngSeed != None):
+			random_seed(prngSeed)
+	else:
+		if (prngSeed not in [None,defaultPrngSeed]):
+			print >>stderr, "WARNING: ignoring request to use PRNG with \"%s\"" \
+			              % testMethod
+
 	# make sure the shell commands we're gonna use have been installed
 
-	if (not shell_command_exists("Rscript")):
-		exit(("%s: Unable to run the shell command \"Rscript\";"
-		       + "\n  .. Either R hasn't been installed, or the command-line shell"
-		       + " can't find it.")
-		   % os_path.basename(argv[0]))
+	if (testMethod == "chi-squared"):
+		if (not shell_command_exists("Rscript")):
+			exit(("%s: Unable to run the shell command \"Rscript\";"
+			       + "\n  .. Either R hasn't been installed, or the command-line shell"
+			       + " can't find it.")
+			   % os_path.basename(argv[0]))
 
 	# collect the alignments; we need to collect the positional info for all
 	# alignments, to feed to R in batches (doing them one-by-one was incredibly
 	# slow); hopefully this won't become a memory problem
 
-	alignmentList = []
-	mxMatrix = []
-	unitLength = None
-
-	alignmentNum = 0
-	for a in alignments(stdin,requireEof):
-		alignmentNum +=1 
-
-		if (headLimit != None) and (alignmentNum > headLimit):
-			print >>stderr, "limit of %d alignments reached" % headLimit
-			break
-
-		if (testWhich == "matches-insertions"):
-			mxRow = positional_error_vector(a,modified="m-i")
-		else:
-			mxRow = positional_error_vector(a)
-		if (mxRow == None):
-			raise ValueError, \
-			      "alignment at line %d does not contain positional information" \
-			    % a.lineNumber
-
-		if (unitLength == None):
-			unitLength = len(mxRow) / 2
-		elif (len(mxRow) != 2*unitLength):
-			raise ValueError, \
-			      "alignments have different motif lengths, %d and %d (detected at line %d)" \
-			    % (unitLength,len(mxRow)/2,a.lineNumber)
-
-		alignmentList += [a]
-		mxMatrix += [mxRow]
+	(unitLength,alignmentList,mxMatrix) \
+	  = collect_alignments(stdin,testWhich,
+	                       headLimit=headLimit,requireEof=requireEof)
 
 	numAlignments = len(alignmentList)
 
@@ -198,21 +220,32 @@ def main():
 			print >>stderr, "using R for alignments %d thru %d" \
 			              % (batchStartIx+1,batchEndIx)
 
-		mxBatch  = mxMatrix[batchStartIx:batchEndIx]
-		sigBatch = mx_significance_tests(mxBatch,testWhich,effectSize,power)
+		mxBatch = mxMatrix[batchStartIx:batchEndIx]
+		aBatch  = alignmentList[batchStartIx:batchEndIx]
 
-		if (type(sigBatch) == str):
-			exit(("%s: internal error: having trouble with R"
-			        + " (with alignment batch %d..%d)"
-			        + "\nHere's what R reported:\n%s")
-			   % (os_path.basename(argv[0]),batchStartIx,batchEndIx,sigBatch))
+		if (testMethod == "chi-squared"):
+			resultBatch = mx_significance_tests(mxBatch,testWhich,effectSize,power)
+			if (type(resultBatch) == str):
+				exit(("%s: internal error: having trouble with R"
+				        + " (with alignment batch %d..%d)"
+				        + "\nHere's what R reported:\n%s")
+				   % (os_path.basename(argv[0]),batchStartIx,batchEndIx,resultBatch))
+		elif (testMethod == "min-max"):
+			resultBatch = min_max_tests(aBatch,mxBatch,testWhich,numTrials)
+			if (type(resultBatch) == str):
+				exit(("%s: internal error: having trouble with min-max test"
+				        + " (with alignment batch %d..%d)"
+				        + "\nHere's what was reported:\n%s")
+				   % (os_path.basename(argv[0]),batchStartIx,batchEndIx,resultBatch))
+		else:
+			exit("%s: internal error: unrecognized test method: \"%s\""
+			   % (os_path.basename(argv[0]),testMethod))
 
-		if (len(sigBatch) != batchEndIx-batchStartIx):
+		if (len(resultBatch) != batchEndIx-batchStartIx):
 			exit(("%s: internal error: number of test outcomes reported by R (%d)"
 			       + "\n  .. doesn't match the number of tests given to R (%d)")
-			   % (os_path.basename(argv[0]),len(sigBatch),batchEndIx-batchStartIx))
-
-		accepted += sigBatch
+			   % (os_path.basename(argv[0]),len(resultBatch),batchEndIx-batchStartIx))
+		accepted += resultBatch
 
 		if (warnOnUntested):
 			for alignmentNum in xrange(batchStartIx,batchEndIx):
@@ -285,6 +318,43 @@ def main():
 
 		if (requireEof):
 			print "# ncrf end-of-file"
+
+
+# collect_alignments--
+
+def collect_alignments(f,testWhich,headLimit=None,requireEof=True):
+	alignmentList = []
+	mxMatrix = []
+	unitLength = None
+
+	alignmentNum = 0
+	for a in alignments(f,requireEof):
+		alignmentNum +=1 
+
+		if (headLimit != None) and (alignmentNum > headLimit):
+			print >>stderr, "limit of %d alignments reached" % headLimit
+			break
+
+		if (testWhich == "matches-insertions"):
+			mxRow = positional_error_vector(a,modified="m-i")
+		else:
+			mxRow = positional_error_vector(a)
+		if (mxRow == None):
+			raise ValueError, \
+			      "alignment at line %d does not contain positional information" \
+			    % a.lineNumber
+
+		if (unitLength == None):
+			unitLength = len(mxRow) / 2
+		elif (len(mxRow) != 2*unitLength):
+			raise ValueError, \
+			      "alignments have different motif lengths, %d and %d (detected at line %d)" \
+			    % (unitLength,len(mxRow)/2,a.lineNumber)
+
+		alignmentList += [a]
+		mxMatrix += [mxRow]
+
+	return (unitLength,alignmentList,mxMatrix)
 
 
 # positional_error_vector--
@@ -419,6 +489,73 @@ def shell_command_exists(commandName):
 	# print >>stderr, err
 
 	return (retCode == 0)
+
+
+# min_max_tests--
+#	"Judge" whether alignments in a batch are uniform or not.
+#
+# Input is an Nx2M matrix, where N is the number of alignments and M is the
+# length of the aligned motif unit.  The first M columns are the positional
+# counts for matches ("m"), and the final M columns are the positional counts
+# for errors ("x").
+#
+# testWhich indicates whether to base the test on the matches or the errors.
+# This is "matches", "errors", or "matches-insertions".
+#
+# If the process was unsuccessful, the return value is a string describing the
+# the reason for the failure (hopefully).
+#
+# Otherwise, output is an N-element vector, containing acceptance/rejection for
+# each row of the input.  Each entry is one of the following:
+#   True  ==> don't reject null hypothesis; the counts are uniform
+#   False ==> reject null hypothesis;       the counts are biased
+#   None  ==> unable to run the test
+
+def min_max_tests(aBatch,mxBatch,testWhich,numTrials):
+	motifLen = len(mxBatch[0]) / 2
+
+	results = [None] * len(mxBatch)
+	for rowIx in xrange(len(mxBatch)):
+		if (testWhich == "errors"):
+			testCounts = mxBatch[rowIx][motifLen:2*motifLen]
+		else:  # this includes "matches" or "matches-insertions"
+			testCounts = mxBatch[rowIx][0:motifLen]
+
+		minCount = min(testCounts)
+		maxCount = max(testCounts)
+
+		numEvents    = sum(testCounts)
+		alignmentLen = len(aBatch[rowIx].seqText)
+
+		minTrialCount = maxTrialCount = None
+		for trialNum in xrange(numTrials):
+			positionToCount = {pos:0 for pos in xrange(motifLen)}
+			motifOffset = randint(0,motifLen-1)
+			for aPos in random_sample(xrange(alignmentLen),numEvents):
+				mPos = (aPos + motifOffset) % motifLen
+				positionToCount[mPos] += 1
+
+			trialMin = min([positionToCount[pos] for pos in xrange(motifLen)])
+			trialMax = max([positionToCount[pos] for pos in xrange(motifLen)])
+
+			if (minTrialCount == None) or (trialMin < minTrialCount):
+				minTrialCount = trialMin
+			if (maxTrialCount == None) or (trialMax < maxTrialCount):
+				maxTrialCount = trialMax
+
+		if (minCount >= minTrialCount) and (maxCount <= maxTrialCount):
+			results[rowIx] = True
+		else:
+			results[rowIx] = False
+
+		if ("min-max" in debug):
+			print >>stderr, "[%d] %d/%d <%s> real:%d..%d random:%d..%d %s" \
+			              % (rowIx,numEvents,alignmentLen,
+			                 " ".join(map(str,testCounts)),
+			                 minCount,maxCount,minTrialCount,maxTrialCount,
+			                 "pass" if (results[rowIx]) else "fail")
+
+	return results
 
 
 # flatten--
