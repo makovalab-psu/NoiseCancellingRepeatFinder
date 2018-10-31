664bc86492b29191c79aa261405e72eda6ce8751
Bob Harris <rsharris@bx.psu.edu>
2018-10-31 12:21:42 -0400

    ncrf_msa not reports a consensus motif derived from the MSA (DOES NOT PROPERLY CONSIDER TIES)

--- ncrf_msa.py
+++ ncrf_msa.py
@@ -11,6 +11,9 @@ from ncrf_parse import alignments,reverse_complement,int_with_unit
 def usage(s=None):
 	message = """
 usage: ncrf_cat <output_from_NCRF> | ncrf_msa [options]
+  --consensus         report the consensus motif
+  --consensusonly     only report the consensus motif
+                      (by default, we report the multiple alignmnent)
   --head=<number>     limit the number of input alignments"""
 
 	if (s == None): exit (message)
@@ -20,17 +23,26 @@ usage: ncrf_cat <output_from_NCRF> | ncrf_msa [options]
 def main():
 	global debug
 
+	closeEnough     = 1  # (see derive_consensus)
+
 	# parse the command line
 
-	headLimit  = None
-	requireEof = True
-	debug      = []
+	reportMsa       = True
+	reportConsensus = False
+	headLimit       = None
+	requireEof      = True
+	debug           = []
 
 	for arg in argv[1:]:
 		if ("=" in arg):
 			argVal = arg.split("=",1)[1]
 
-		if (arg.startswith("--head=")):
+		if (arg == "--consensus"):
+			reportConsensus = True
+		elif (arg == "--consensusonly"):
+			reportMsa       = False
+			reportConsensus = True
+		elif (arg.startswith("--head=")):
 			headLimit = int_with_unit(argVal)
 		elif (arg in ["--noendmark","--noeof","--nomark"]):   # (unadvertised)
 			requireEof = False
@@ -67,28 +79,33 @@ def main():
 
 		seqChunks = chunkify(a.motif,motifText,seqText)
 
-		motifLen = len(a.motif)
-		positionLength = [1] * motifLen
-		for chunk in seqChunks:
-			for (motifIx,seqNucs) in enumerate(chunk):
-				if (seqNucs == None): continue
-				positionLength[motifIx] = max(positionLength[motifIx],len(seqNucs))
+		if (reportMsa):
+			motifLen = len(a.motif)
+			positionLength = [1] * motifLen
+			for chunk in seqChunks:
+				for (motifIx,seqNucs) in enumerate(chunk):
+					if (seqNucs == None): continue
+					positionLength[motifIx] = max(positionLength[motifIx],len(seqNucs))
 
-		line = []
-		for (motifIx,motifNuc) in enumerate(a.motif):
-			line += [motifNuc.ljust(positionLength[motifIx],".")]
-		print "# msa.query %s" % "".join(line)
-
-		for chunk in seqChunks:
 			line = []
-			for (motifIx,seqNucs) in enumerate(chunk):
-				if (seqNucs == None):
-					line += ["."*positionLength[motifIx]]
-				elif (seqNucs == a.motif[motifIx]):
-					line += ["="*positionLength[motifIx]]
-				else:
-					line += [seqNucs.ljust(positionLength[motifIx],".")]
-			print "# msa.seq   %s" % "".join(line)
+			for (motifIx,motifNuc) in enumerate(a.motif):
+				line += [motifNuc.ljust(positionLength[motifIx],".")]
+			print "# msa.query %s" % "".join(line)
+
+			for chunk in seqChunks:
+				line = []
+				for (motifIx,seqNucs) in enumerate(chunk):
+					if (seqNucs == None):
+						line += ["."*positionLength[motifIx]]
+					elif (seqNucs == a.motif[motifIx]):
+						line += ["="*positionLength[motifIx]]
+					else:
+						line += [seqNucs.ljust(positionLength[motifIx],".")]
+				print "# msa.seq   %s" % "".join(line)
+
+		if (reportConsensus):
+			for word in derive_consensus(seqChunks,closeEnough=closeEnough):
+				print "# consensus %s" % word
 
 	if (requireEof):
 		print "# ncrf end-of-file"
@@ -110,9 +127,6 @@ def chunkify(motif,motifText,seqText):
 	chunk = [None] * motifIx
 	for (ix,textNuc) in enumerate(motifText):
 		seqNuc = seqText[ix]
-#...
-#		print >>stderr, seqNuc,textNuc
-#...
 		if (textNuc == "-"):     # insertion, extra character in seqText
 			if (chunk != []):    # add inserted character to latest token
 				if (chunk[-1] != None): chunk[-1] += seqNuc
@@ -123,9 +137,6 @@ def chunkify(motif,motifText,seqText):
 			continue
 
 		chunk += [seqNuc]        # match or deletion
-#...
-#		print >>stderr, chunk
-#...
 		motifIx += 1
 		if (motifIx == motifLen):
 			chunks += [chunk]
@@ -193,6 +204,62 @@ def position_in_motif(motif,text):
 	# no match found, just report position 0,forward
 
 	return (0,"+")
+
+
+# derive_consensus--
+#	Yields a series of potential consensus motifs for a given alignment. The
+#	input is a list of sublists (as created by chunkify).  Each sublist is as
+#	long as the motif, consisting of the string (the nt or nts) matched to each
+#	position in the motif.
+
+def derive_consensus(seqChunks,closeEnough=0):
+	if (seqChunks == []): return
+	motifLen = len(seqChunks[0])
+
+	# count the number of times each motif position is observed to match a
+	# particular 'token'
+
+	ixToTokens = {}
+	for motifIx in xrange(motifLen):
+		ixToTokens[motifIx] = {}
+
+	for chunk in seqChunks:
+		for (motifIx,seqNucs) in enumerate(chunk):
+			if (seqNucs == None): seqNucs = ""
+			if (seqNucs not in ixToTokens[motifIx]):
+				ixToTokens[motifIx][seqNucs] = 1
+			else:
+				ixToTokens[motifIx][seqNucs] += 1
+
+	# at each position, sort the tokens from most-observed to least-observed,
+	# then reduce them to best (or almost tied for best)
+
+	for motifIx in xrange(motifLen):
+		tokensSeen = ixToTokens[motifIx]
+		ixToTokens[motifIx] = [(ixToTokens[motifIx][seqNucs],seqNucs) for seqNucs in tokensSeen]
+		ixToTokens[motifIx].sort()
+		ixToTokens[motifIx].reverse()
+
+		(bestCount,_) = ixToTokens[motifIx][0]
+
+		ixToTokens[motifIx] = [ixToTokens[motifIx][ix]
+		                         for (ix,(count,_)) in enumerate(ixToTokens[motifIx])
+		                         if (count >= bestCount-closeEnough)]
+
+		if ("consensus" in debug):
+			for (count,seqNucs) in ixToTokens[motifIx]:
+				print >>stderr, "# [%d] %d \"%s\"" % (motifIx,count,seqNucs)
+			print >>stderr, "# %d" % bestCount
+
+	# generate the possible consensus motifs
+	# $$$ change this to include the almost-tied ones
+
+	motif = []
+	for motifIx in xrange(motifLen):
+		(_,bestSeqNucs) = ixToTokens[motifIx][0]
+		motif += [bestSeqNucs]
+
+	yield "".join(motif)
 
 
 if __name__ == "__main__": main()
