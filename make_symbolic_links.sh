160e6cc52e71c9fe341fa4b064a99936c8984cde
Bob Harris <rsharris@bx.psu.edu>
2026-04-02 11:25:47 -0400

    adding make_symbolic_links.sh, which was indavertently omitted 8 years ago

---
+++ make_symbolic_links.sh
@@ -0,0 +1,6 @@
+ln -s ncrf_cat.py               ncrf_cat
+ln -s ncrf_consensus_filter.py  ncrf_consensus_filter
+ln -s ncrf_sort.py              ncrf_sort
+ln -s ncrf_summary.py           ncrf_summary
+ln -s ncrf_to_bed.py            ncrf_to_bed
+ln -s ncrf_resolve_overlaps.py  ncrf_resolve_overlaps
